c583453383d6d137c4ed8567c95ff7263a19df2c
ptkprateeksaxena <ptksna1@gmail.com>
2026-08-30 16:16:02 +0530

    30/08/26

---
+++ chapter_10_loop/10_03_while_loop.js
@@ -0,0 +1,11 @@
+let attempt =0;
+while(attempt<3){
+    console.log(attempt)
+    attempt++
+}
+
+let modi =1;
+while (modi <=10){
+    console.log("modi hai to sarkar kharab hai ", modi)
+    modi++;
+}
---
+++ chapter_10_loop/10_04_do_while.js
@@ -0,0 +1,9 @@
+let a = 15;
+
+do {
+    console.log(a);
+    a++
+} while (a < 20)
+
+    // it always execute atlest one time
+
